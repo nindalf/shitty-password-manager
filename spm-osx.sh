82262d95812e52ae4672c4fb251d4092f128ddc1
Krishna Sundarram <krishna@zeta.in>
2016-06-15 20:31:37 +0530

    Add ability to edit the password file

--- spm-osx.sh
+++ spm-osx.sh
@@ -2,7 +2,7 @@ ENCRYPTED_PASSWORD_FILE=~/cipher
 CLEAR_PASSWORD=true
 PASSWORD_CLEAR_TIME=10
 
-passwordmanager() {
+passwordGet() {
   openssl aes-256-cbc -in $ENCRYPTED_PASSWORD_FILE -d | grep $1 | awk '{print $NF}' | pbcopy
   echo "Password for" $1 "copied to clipboard"
   if [ $CLEAR_PASSWORD = true ] ; then
@@ -12,4 +12,15 @@ passwordmanager() {
   fi
 }
 
-alias pass=passwordmanager
+passwordSet() {
+  if [ -f $ENCRYPTED_PASSWORD_FILE ];
+  then
+    textfilecontents=$(openssl aes-256-cbc -in $ENCRYPTED_PASSWORD_FILE -d | vipe)
+  else
+    textfilecontents=$(echo "" | vipe)
+  fi
+  echo "$textfilecontents" | openssl aes-256-cbc -out $ENCRYPTED_PASSWORD_FILE
+}
+
+alias pass=passwordGet
+alias passedit=passwordSet
